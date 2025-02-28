3c220a2a61abbecb245e395c7dac00b9eb9a1446
ardszsantos <lokgamer01@gmail.com>
2025-02-28 15:17:15 -0300

    loader

---
+++ popup.js
@@ -0,0 +1,8 @@
+document.getElementById('select').addEventListener('click', async () => {
+  let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ['content.js']
+  });
+});
+
